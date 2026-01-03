c720ee094de98a6500ac09a0b461d987929f0f76
Gabriel Scherer <schererg03@gmail.com>
2026-01-03 22:00:14 +0100

    Added T=1 analysis for area per lipid and water to lipid ratio, cleaned up power spectrum code, writing backup

---
+++ lipid_self_assembly/apl_opt/trajs_T_1/show.tcl
@@ -0,0 +1,23 @@
+# Load trajectories
+mol new bilayer_05.lammpstrj type lammpstrj waitfor all
+
+mol delrep 0 top
+
+# White Background
+color Display Background white
+display projection Perspective
+axes location Off
+
+
+# Color and Shape
+mol representation VDW 0.1 30
+mol color Type
+mol material AOShiny
+mol addrep top
+
+color Type 4 23
+color Type 1 10
+color Type 2 6
+color Type 3 2
+color Type 5 30
+color Type 6 3
